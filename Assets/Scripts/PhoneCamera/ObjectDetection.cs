b176dc5e012fa5e07946ef82ac5ede6e2df9de5b
CurryKing123 <isaac.park@hotmail.com>
2024-10-11 16:28:27 -0400

    name

--- Assets/Scripts/PhoneCamera/ObjectDetection.cs
+++ Assets/Scripts/PhoneCamera/ObjectDetection.cs
@@ -74,7 +74,7 @@ public class ObjectDetection : MonoBehaviour
             categorizations.Sort((a, b) => b.Confidence.CompareTo(a.Confidence));
             var categoryToDisplay = categorizations[0];
             confidence = categoryToDisplay.Confidence;
-            name = categoryToDisplay.CategoryName;
+            rectName = categoryToDisplay.CategoryName;
 
             int h = Mathf.FloorToInt(canvas.GetComponent<RectTransform>().rect.height);
             int w = Mathf.FloorToInt(canvas.GetComponent<RectTransform>().rect.width);
